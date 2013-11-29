3da55626bc2b6b3dfbc7741b59d9ac2f2284c60a
Syed Murtaza Mairaj Alvi <smurtazama@gmail.com>
2013-11-29 16:30:05 +0530

    Finish user edit, update, index actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Murtaza Alvi"
-    email    "smurtazama@gmail.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@jsschool.org"}
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
